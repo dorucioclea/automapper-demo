ad9d5aa9cbbae536027d18e0fe98be4abc8b1b43
Cioclea Doru <doru.cioclea@corebuild.eu>
2020-04-28 15:08:52 +0300

    chore(app): Tidy up

--- Modules/ServiceModule.cs
+++ Modules/ServiceModule.cs
@@ -1,12 +1,12 @@
 using Autofac;
-using AutofacDemoConsole;
 using AutofacDemoConsole.Services;
 
-public class ServicesModule : Module
-{
-    protected override void Load(ContainerBuilder builder) 
+namespace AutofacDemoConsole {
+    public class ServicesModule : Module 
     {
-        builder.RegisterType<TestService>().As<ITestService>().InstancePerLifetimeScope();
-        builder.RegisterType<App>().SingleInstance();
+        protected override void Load (ContainerBuilder builder) {
+            builder.RegisterType<TestService> ().As<ITestService> ().InstancePerLifetimeScope ();
+            builder.RegisterType<App> ().SingleInstance ();
+        }
     }
 }
